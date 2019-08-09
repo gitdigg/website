95e1c58561fda5a2922f366e8d5fec28dc31649f
JayL <liujianping.itech@qq.com>
2019-08-09 17:40:58 +0800

    add tcp/udp server projects

--- data/projects.js
+++ data/projects.js
@@ -1,6 +1,20 @@
 const projects = [
   {
     icon: '⛏️',
+    title: 'tcpserver',
+    path: 'https://github.com/x-mod/tcpserver',
+    source: 'https://github.com/x-mod/tcpserver',
+    description: 'tcpserver, a very simple & convenient tcp server framework',
+  },
+  {
+    icon: '⛏️',
+    title: 'thriftudp',
+    path: 'https://github.com/x-mod/thriftudp',
+    source: 'https://github.com/x-mod/thriftudp',
+    description: 'udpserver, a very simple udpserver framework with thrift support',
+  },
+  {
+    icon: '⛏️',
     title: 'ts',
     path: 'https://github.com/liujianping/ts',
     source: 'https://github.com/liujianping/ts',

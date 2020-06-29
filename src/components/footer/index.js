6fb15261ded1d3fabcb80ddfebd7318fef517e57
liujianping <liujianping.itech@qq.com>
2020-06-29 19:29:55 +0800

    add repo lonk & content update

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -8,7 +8,8 @@ export function Footer({ className }) {
                 <ul>
                     <li><Link to="/doc/announce">公告</Link></li>
                     <li><Link to="/doc/copyright">版权</Link></li>
-                    <li><Link to="/keywords">关键字</Link></li>
+                    {/* <li><Link to="/keywords">关键字</Link></li> */}
+                    <li><a aria-label={'opensource'} target="_blank" rel="nofollow noopener noreferrer" href="https://github.com/gitdigg/website">开源</a></li>
                     <li><Link to="/about">关于</Link></li>
                 </ul>
                 <p className="has-text-centered has-text-grey-dark"> copyright @ 2020 powered by <a target="_blank" rel="noreferrer" href={'https://netlify.com/'}>netlify</a></p>

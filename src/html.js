cfecc50689615ed1ae92f503aa43e5fb0698d560
liujianping <liujianping.itech@qq.com>
2020-07-10 23:58:22 +0800

    update index

--- src/html.js
+++ src/html.js
@@ -7,7 +7,7 @@ export default function HTML(props) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta name="referrer" content="no-referrer-when-downgrade">            
+        <meta name="referrer" content="no-referrer-when-downgrade" />            
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
